1d1b037f6bd6e7cd38192af21afb3c8db5c4a146
user <me@mail.com>
2020-10-03 20:35:53 -0700

    Initial Commit

---
+++ TwelveFlags/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  TwelveFlags
+//
+//  Created by RnD on 10/3/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+    
+    var body: some View {
+        ZStack {
+            Color.blue.edgesIgnoringSafeArea(.all)
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                }
+
+            
+                ForEach(0 ..< 3) { number in
+                    Button(action: {
+                        
+                    }) {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                    }
+                }
+                Spacer()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TwelveFlags/TwelveFlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwelveFlagsApp.swift
+//  TwelveFlags
+//
+//  Created by RnD on 10/3/20.
+//
+
+import SwiftUI
+
+@main
+struct TwelveFlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
