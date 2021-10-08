e256d51f746c1174e26e039628e551fc9e3e53db
user <43841845+eurhythmic@users.noreply.github.com>
2021-10-07 17:39:54 -0700

    Add form validation

--- TwelveFlags/Score/SubmitScoreViewModel.swift
+++ TwelveFlags/Score/SubmitScoreViewModel.swift
@@ -6,9 +6,34 @@
 //
 
 import Foundation
+import Combine
 
 class SubmitScoreViewModel: ObservableObject {
     @Published var playerName = ""
-    @Published var buttonTapped = false
+    @Published var scoreSent = false
+    var textFieldLabel: String {
+        scoreSent ? "SUBMITTED" : "ENTER 1UP INITIALS!"
+    }
     let characterLimit = 25
+    var cancellable: AnyCancellable?
+    
+    init() {
+        trimTextFieldCharacters()
+    }
+}
+
+private extension SubmitScoreViewModel {
+    /// Trims whitespace from the beginning of the text field value
+    func trimTextFieldCharacters() {
+        cancellable = $playerName
+            .sink {
+                if $0.first == " " {
+                    let newValue = String($0.dropFirst())
+
+                    DispatchQueue.main.async {
+                        self.playerName = newValue
+                    }
+                }
+            }
+    }
 }
