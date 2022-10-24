ee4c0de4024ccdc03664d9122e5519e94bb6c0a0
user <43841845+eurhythmic@users.noreply.github.com>
2022-10-23 17:20:20 -0700

    Add last active stat

--- TwelveFlags/TwelveFlagsApp.swift
+++ TwelveFlags/TwelveFlagsApp.swift
@@ -10,10 +10,25 @@ import SwiftUI
 @main
 struct TwelveFlagsApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    @Environment(\.scenePhase) private var scenePhase
+    @StateObject var settingsViewModel = SettingsViewModel()
     
     var body: some Scene {
         WindowGroup {
             StartView()
+                .environmentObject(settingsViewModel)
+        }
+        .onChange(of: scenePhase) { phase in
+            switch phase {
+            case .active:
+                settingsViewModel.settings.lastActive = Date.now
+            case .inactive:
+                break
+            case .background:
+                break
+            @unknown default:
+                break
+            }
         }
     }
 }
